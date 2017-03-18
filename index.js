a074fd894e4081bb1bebe8aff57b21f059d063bb
Geoff Kimball <geoff@geoffkimball.com>
2017-03-17 17:38:46 -0700

    Finish later

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var format = require('util').format;
+var urlRegex = require('url-regex');
 
 // Thank you: http://stackoverflow.com/a/32382702 (with some modifications)
-var linkRegExp = /\[([^\]]+)?\]\(([^\)]+)\)/g
+var linkRegExp = /\[([^\]]+)?\]\(([^\)]+)\)/g;
+var aliasLinkRegExp = /^\[(.*)\]:\s+(.*)(".*"|'.*'|\(.*\))?$/gm;
 
 /**
  * Transform the links in a Markdown string using a transform function.
@@ -23,6 +25,10 @@ module.exports = function transformMarkdownLinks(input, transform) {
   // match[1] is the text, without the brackets
   // match[2] is the URL, without the parentheses
   while ((match = linkRegExp.exec(input)) !== null) {
+    if (match[1] === undefined) {
+      match[1] = "";
+    }
+
     /**
      * Callback to transform a link within a Markdown string.
      * @callback TransformCallback
@@ -30,13 +36,34 @@ module.exports = function transformMarkdownLinks(input, transform) {
      * @param {String} text - Text of the link.
      * @returns {?String} Modified URL.
      */
-     if (match[1] === undefined) {
-       match[1] = "";
-     }
     var replacement = transform(match[2], match[1]);
 
     if (replacement) {
       output = output.replace(match[0], format('[%s](%s)', match[1], replacement));
+    }
+  }
+
+  // match[0] is the entire link
+  // match[1] is the alias name
+  // match[2] is the URL
+  // match[3] is the title
+  while ((match = aliasLinkRegExp.exec(input)) !== null) {
+    var string = match[0];
+    var alias = match[1];
+    var url = match[2];
+    var title = match[3];
+    var urlBrackets = /^<.*>$/.test(url);
+    var innerUrl = urlBrackets
+      ? url.replace(/^</, '').replace(/>$/, '')
+      : url;
+
+    var replacement = transform(innerUrl, alias);
+    if (urlBrackets) {
+      replacement = '<' + replacement + '>';
+    }
+
+    if (replacement) {
+      output = output.replace(string, format('[%s]: %s', alias, replacement));
     }
   }
 
