e9a481b4a009e08b11339f9afe480542897adbac
Geoff Kimball <geoff@geoffkimball.com>
2017-03-19 14:25:20 -0700

    Update to Node 4+, use ES2015, add xo for linting

--- test.js
+++ test.js
@@ -1,49 +1,54 @@
-var transform = require('./');
-var expect = require('chai').expect;
+/* eslint import/no-unresolved:0 */
+/* eslint-env mocha */
 
-describe('transform()', function() {
-  it('transforms Markdown links', function() {
-    var input = 'There\'s a [link](page.md) or [two](page2.md) in this sentence.';
-    var output = transform(input, function(link, text) {
+'use strict';
+
+const expect = require('chai').expect;
+const transform = require('.');
+
+describe('transform()', () => {
+  it('transforms Markdown links', () => {
+    const input = 'There\'s a [link](page.md) or [two](page2.md) in this sentence.';
+    const output = transform(input, link => {
       return link.replace('md', 'html');
     });
     expect(output).to.equal('There\'s a [link](page.html) or [two](page2.html) in this sentence.');
   });
 
-  it('calls the transform function with the URL and text of the link', function() {
-    transform('[text](url)', function(link, text) {
+  it('calls the transform function with the URL and text of the link', () => {
+    transform('[text](url)', (link, text) => {
       expect(link).to.equal('url');
       expect(text).to.equal('text');
     });
   });
 
-  it('detects empty text links', function() {
-    var input = '![](/resources/picture.JPG) Here is a link with no text.'
-    var output = transform(input, function(link, text) {
+  it('detects empty text links', () => {
+    const input = '![](/resources/picture.JPG) Here is a link with no text.';
+    const output = transform(input, () => {
       return 'http://url.to/picture';
     });
-    expect(output).to.equal('![](http://url.to/picture) Here is a link with no text.')
+    expect(output).to.equal('![](http://url.to/picture) Here is a link with no text.');
   });
 
-  it('matches link aliases', function() {
-    var input = '[This link][alias] is an alias.\n\n[alias]: http://example.com';
-    var output = transform(input, function(link, text) {
+  it('matches link aliases', () => {
+    const input = '[This link][alias] is an alias.\n\n[alias]: http://example.com';
+    const output = transform(input, () => {
       return 'http://example.org';
     });
     expect(output).to.equal('[This link][alias] is an alias.\n\n[alias]: http://example.org');
   });
 
-  it('matches link aliases with a link enclosed in angle brackets', function() {
-    var input = '[This link][alias] is an alias.\n\n[alias]: <http://example.com>';
-    var output = transform(input, function(link, text) {
+  it('matches link aliases with a link enclosed in angle brackets', () => {
+    const input = '[This link][alias] is an alias.\n\n[alias]: <http://example.com>';
+    const output = transform(input, () => {
       return 'http://example.org';
     });
     expect(output).to.equal('[This link][alias] is an alias.\n\n[alias]: <http://example.org>');
   });
 
-  it('matches link aliases with a title', function() {
-    var input = '[This link][alias] is an alias.\n\n[alias]: http://example.com (title)';
-    var output = transform(input, function(link, text) {
+  it('matches link aliases with a title', () => {
+    const input = '[This link][alias] is an alias.\n\n[alias]: http://example.com (title)';
+    const output = transform(input, () => {
       return 'http://example.org';
     });
     expect(output).to.equal('[This link][alias] is an alias.\n\n[alias]: http://example.org (title)');
